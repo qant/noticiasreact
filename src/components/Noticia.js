9c5a217d52d91c204e2a3aff27353dadf9488a0c
Anton Semenov <anseme@gmail.com>
2019-07-28 17:10:43 +0200

    Nocical extract values and show

--- src/components/Noticia.js
+++ src/components/Noticia.js
@@ -1,7 +1,31 @@
 import React from "react";
 
-const Noticia = () => {
-  return <div>Noticia</div>;
+const Noticia = ({ noticia }) => {
+  const { urlToImage, url, description, title } = noticia;
+
+  return (
+    <div className="col s12 m6 l4">
+      <div className="card">
+        <div className="card-image">
+          <img src={urlToImage} alt={title} />
+        </div>
+        <div className="card-content">
+          <h3>{title}</h3>
+          {description}
+        </div>
+        <div className="card-action">
+          <a
+            href={url}
+            target="_blank"
+            className="btn"
+            rel="noopener noreferrer"
+          >
+            Leer mas
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Noticia;
