e6c2a923facd20ccfbc2de6c134ac36a34583522
Anton Semenov <anseme@gmail.com>
2019-07-28 14:23:38 +0200

    Noticias react

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { Component, Fragment } from "react";
+import Header from "./components/Header";
+
+class App extends Component {
+  state = {};
+
+  componentDidMount() {
+    this.consultarNoticias();
+  }
+
+  consultarNoticias = async () => {
+    const url = `https://newsapi.org/v2/top-headlines?country=ru&category=business&apiKey=b7aa048b86df4122bfc4c7cacde795a0`;
+
+    const response = await fetch(url);
+    const noticias = await response.json();
+    this.setState({
+      noticias: noticias.articles
+    });
+    //console.log(noticias.articles);
+  };
+
+  render() {
+    return (
+      <Fragment>
+        <Header title="Noticias" />
+      </Fragment>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React, { Component, Fragment } from "react";
+
+const Header = ({ title }) => (
+  <nav className="nav-wrapperlight-blue darken-3">
+    <a className="brqand-logo center">{title}</a>
+  </nav>
+);
+
+export default Header;
