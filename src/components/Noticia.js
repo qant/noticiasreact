0ddf5bc6eefa9abf6a1da88f60f04392d8c005fb
Anton Semenov <anseme@gmail.com>
2019-07-28 17:16:27 +0200

    If no image and source name on image

--- src/components/Noticia.js
+++ src/components/Noticia.js
@@ -1,14 +1,19 @@
 import React from "react";
 
 const Noticia = ({ noticia }) => {
-  const { urlToImage, url, description, title } = noticia;
+  const { urlToImage, url, description, title, source } = noticia;
+
+  const image = urlToImage ? (
+    <div className="card-image">
+      <img src={urlToImage} alt={title} />
+      <span className="card-title">{source.name}</span>
+    </div>
+  ) : null;
 
   return (
     <div className="col s12 m6 l4">
       <div className="card">
-        <div className="card-image">
-          <img src={urlToImage} alt={title} />
-        </div>
+        {image}
         <div className="card-content">
           <h3>{title}</h3>
           {description}
